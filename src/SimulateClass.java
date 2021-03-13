159fb7e337dc983dd20d44b4409754c37fb2b795
corneliacalin <corneliacalin@yahoo.com>
2021-03-13 14:19:15 +0200

    gitignore

--- src/SimulateClass.java
+++ src/SimulateClass.java
@@ -12,24 +12,24 @@ public class SimulateClass {
         private static void createClass() {
             Student student1 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
             System.out.println(student1);
-            Student student2 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student3 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student4 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student5 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student6 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student7 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student8 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student9 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
-            Student student10 = new Student("Cornelia", "Calin", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student2 = new Student("Ana", "Popa", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student3 = new Student("Andreea", "Breaz", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student4 = new Student("Maria", "Popescu", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student5 = new Student("Bianca", "Ionescu", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student6 = new Student("Vasile", "Petrescu", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student7 = new Student("Gheorghe", "Iliescu", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student8 = new Student("Ioan", "Rotar", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student9 = new Student("Emil", "Radu", LocalDate.of(1974, Month.AUGUST, 28), false);
+            Student student10 = new Student("Marius", "Muntean", LocalDate.of(1974, Month.AUGUST, 28), false);
 
             List<Student> studentList1 = Arrays.asList(student1, student2, student3);
             List<Student> studentList2 = Arrays.asList(student4, student5, student6);
             List<Student> studentList3 = Arrays.asList(student7, student8, student9);
             List<Student> studentList4 = Arrays.asList(student10);
 
-            Trainer trainer1 = new Trainer("Popa", "ion", LocalDate.of(1974, Month.AUGUST, 28), true);
-            Trainer trainer2 = new Trainer("Gheorghe", "ion", LocalDate.of(1974, Month.AUGUST, 28), true);
-            Trainer trainer3 = new Trainer("Vasile", "ion", LocalDate.of(1974, Month.AUGUST, 28), true);
+            Trainer trainer1 = new Trainer("Popa", "Ilie", LocalDate.of(1974, Month.AUGUST, 28), true);
+            Trainer trainer2 = new Trainer("Gheorghe", "Vasile", LocalDate.of(1974, Month.AUGUST, 28), true);
+            Trainer trainer3 = new Trainer("Vasile", "Viorel", LocalDate.of(1974, Month.AUGUST, 28), true);
 
 
             Group JavaRo16 = new Group(trainer1, studentList1);
